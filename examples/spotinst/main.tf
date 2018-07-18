8765626d34dec4f52614491b74e99316e4b18a00
beniishak <wahb.bi@mobfox.com>
2018-07-18 18:28:17 +0200

    update example to show example of accessing map keys

--- examples/spotinst/main.tf
+++ examples/spotinst/main.tf
@@ -19,3 +19,11 @@ JSON
 output "resources" {
   value = "${multiverse_custom_resource.spotinst_targetset_and_rules.id}"
 }
+
+output "test_targetset_id" {
+  value = "${multiverse_custom_resource.spotinst_targetset_and_rules["testTargetSet"]}"
+}
+
+output "control_targetset_id" {
+  value = "${multiverse_custom_resource.spotinst_targetset_and_rules["controlTargetSet"]}"
+}
